da8e3b3a42b91ff45fc7e18ae3905bb720813d78
solvman <solvman@gmail.com>
2023-02-12 13:59:09 -0600

    finilized

---
+++ scripts/withdraw.js
@@ -0,0 +1,15 @@
+const { getNamedAccounts, ethers } = require("hardhat");
+
+async function main() {
+    const { deployer } = await getNamedAccounts();
+    const fundMe = await ethers.getContract("FundMe", deployer);
+    console.log("Withdrawing from Contract...");
+    const transactionResponse = await fundMe.withdraw();
+    await transactionResponse.wait(1);
+    console.log("Withdrawn!");
+}
+
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
